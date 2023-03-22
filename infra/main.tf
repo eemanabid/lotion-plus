25e4ad0dbab835606fa3bcc0246db340db922567
eemanabid <abideeman@gmail.com>
2023-03-21 21:27:49 -0600

    added more to terraform file

--- infra/main.tf
+++ infra/main.tf
@@ -15,15 +15,10 @@ provider "aws" {
 # the locals block is used to declare constants that 
 # you can use throughout your code
 locals {
-  function_name = "hello-world"
-  handler_name  = "main.handler"
-  artifact_name = "${local.function_name}/artifact.zip"
+  function_name = "save-note"
+  handler_name  = "main.lambda_handler"
+  artifact_name = "artifact.zip"
 }
-
-# Create an S3 bucket
-# if you omit the name, Terraform will assign a random name to it
-# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "lambda" {}
 
 # create a role for the Lambda function to assume
 # every service on AWS that wants to call other AWS services should first assume a role and
@@ -49,15 +44,23 @@ resource "aws_iam_role" "lambda" {
 EOF
 }
 
+# create archive file from main.py
+data "archive_file" "lambda" {
+  type = "zip"
+  # this file (main.py) needs to exist in the same folder as this 
+  # Terraform configuration file
+  source_file = "main.py"
+  output_path = "artifact.zip"
+}
+
 # create a Lambda function
 # see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function
 resource "aws_lambda_function" "lambda" {
-  s3_bucket = aws_s3_bucket.lambda.bucket
-  # the artifact needs to be in the bucket first. Otherwise, this will fail.
-  s3_key        = local.artifact_name
-  role          = aws_iam_role.lambda.arn
-  function_name = local.function_name
-  handler       = local.handler_name
+  role             = aws_iam_role.lambda.arn
+  function_name    = local.function_name
+  handler          = local.handler_name
+  filename         = local.artifact_name
+  source_code_hash = data.archive_file.lambda.output_base64sha256
 
   # see all available runtimes here: https://docs.aws.amazon.com/lambda/latest/dg/API_CreateFunction.html#SSS-CreateFunction-request-Runtime
   runtime = "python3.9"
@@ -94,7 +97,50 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   policy_arn = aws_iam_policy.logs.arn
 }
 
-# output the name of the bucket after creation
-output "bucket_name" {
-  value = aws_s3_bucket.lambda.bucket
+# create a Function URL for Lambda 
+# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function_url
+resource "aws_lambda_function_url" "url" {
+  function_name      = aws_lambda_function.lambda.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["GET", "POST", "PUT", "DELETE"]
+    allow_headers     = ["*"]
+    expose_headers    = ["keep-alive", "date"]
+  }
+}
+
+# show the Function URL after creation
+output "lambda_url" {
+  value = aws_lambda_function_url.url.function_url
+}
+
+# read the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table
+resource "aws_dynamodb_table" "notes" {
+  name         = "notes"
+  billing_mode = "PROVISIONED"
+
+  # up to 8KB read per second (eventually consistent)
+  read_capacity = 1
+
+  # up to 1KB per second
+  write_capacity = 1
+
+  # we only need a student id to find an item in the table; therefore, we 
+  # don't need a sort key here
+  hash_key = "email"
+  range_key = "id"
+
+  # the hash_key data type is string
+  attribute {
+    name = "email"
+    type = "S"
+  }
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
 }
